9885c185bb30ce87f286b7c3007dc695963a30ab
Alumno <Alumno@a10pc12>
2019-01-31 12:33:02 +0100

    segundo

--- pruebaGit1/src/pruebagit1/PruebaGit1.java
+++ pruebaGit1/src/pruebagit1/PruebaGit1.java
@@ -7,6 +7,10 @@ public class PruebaGit1 {
     
     public static void main(String[] args) {
         System.out.println("olaf");
+        if(true)
+            System.out.println("caca");
+        else
+            System.out.println("pipi");
     }
     
 }
